343a958d76120cddfa6b53a8340b20253a1c6067
artursab <arturs.belavskis@gmail.com>
2023-10-22 15:12:42 +0200

    tograyscale

--- ASCII_art/Program.cs
+++ ASCII_art/Program.cs
@@ -36,7 +36,7 @@ namespace ASCII_art
                 var bitmap = new Bitmap(openFileDialog.FileName);
                 bitmap = ResizeBitmap(bitmap);
 
-                char[] _asciiTable = { '.', ',',':','+','*','?','%','S','#','@' };
+                
                 
             }
         }
